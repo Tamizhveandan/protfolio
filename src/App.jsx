85153b84bd2c977abe19ff0998d10714daa013f0
tamizhveandan <tamizhveandan@gmail.com>
2023-04-17 20:22:47 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter } from "react-router-dom";
+import {About,Contact,Experience , Feedbacks ,Hero , Navbar ,
+   Tech , Works , StarsCanvas} from './components'
 
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div>hi</div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-centre">
+          <Navbar/>
+          <Hero />
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
+   
   )
 }
 
